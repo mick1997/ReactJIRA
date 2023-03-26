280a0e28c340e0440308ff1f4bd17b3b2ebdcc9b
mick1997 <mickjack2013@gmail.com>
2023-03-26 18:30:06 -0400

    add useArray custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ProjectListScreen } from './screens/project-list';
+import { TsReactTest } from 'screens/project-list/try-use-array';
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      <TsReactTest/>
+      {/* <ProjectListScreen/> */}
     </div>
   );
 }
